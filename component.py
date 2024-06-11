ea993aeabcebd96300b844b1e39089598f66ada2
ryanho <horuixiang@gmail.com>
2024-06-11 10:36:09 +0800

    update on component py

--- component.py
+++ component.py
@@ -283,10 +283,10 @@ compiler.Compiler().compile(pipeline_func=income_pipeline, package_path="income.
 from kfp.client import Client
 
 
-KUBEFLOW_ENDPOINT = "http://194.233.80.15:8080"
+KUBEFLOW_ENDPOINT = "http://194.233.80.15:8090"
 KUBEFLOW_USERNAME = "user@example.com"
 KUBEFLOW_PASSWORD = "12341234"
-
+ 
 auth_session = get_istio_auth_session(
     url=KUBEFLOW_ENDPOINT,
     username=KUBEFLOW_USERNAME,
